bfa7d84b6a3ec8b47a21718407b1d6c01f36cf97
shantanujainx <shantanu@rhythmflows.com>
2023-08-04 12:01:54 +0530

    Added to Minactivity

--- app/src/main/java/com/example/mylibraryjava/MainActivity.java
+++ app/src/main/java/com/example/mylibraryjava/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.mylibraryjava;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mylibrary.DataDisplayManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DataDisplayManager.displayData(this,"SHantanu", 10);
+
     }
 }
